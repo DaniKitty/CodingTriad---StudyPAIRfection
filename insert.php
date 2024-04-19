8b073d4758fb4ac89ee6c83589e6adf49c5cf64a
Benita-Jennifer <76103209+Benita-Jennifer@users.noreply.github.com>
2024-04-19 23:14:12 +0530

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,26 +1,22 @@
 <?php
 $username = $_POST['username'];
-$password = $_POST['age'];
+$age = $_POST['age'];
 $email = $_POST['email'];
-$Subjects = $_POST['subjects'];
+$subjects = $_POST['subjects'];
 echo $username;
-echo $password;
+echo $age;
 echo $email;
-echo $Subjects;
-if(!empty($username)|| !empty($password)|| !empty($email)|| !empty($subjects)){
-   $host = "192.168.1.13";
-   $dbUsername = "studybuddynew";
-   $dbPassword = "studybuddynew";
-   $dbname = "studybuddynew";
+echo $subjects;
+if(!empty($username)|| !empty($age)|| !empty($email)|| !empty($subjects)){
    
-   $conn = new mysqli($host, $dbusername, $dbPassword, $dbname);
+   $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 
    if(mysqli_connect_error()){
     die('Connect Error('. mysqli_connect_error().')'. mysqli_connect_error());
    }
    else{
-    $SELECT = "SELECT email From register Where email = ? Limit 1";
-    $INSERT = "INSERT Into register(username, password, email, subjects) values(?, ?, ?, ?)";
+    $SELECT = "SELECT email From student Where email = ? Limit 1";
+    $INSERT = "INSERT Into student(username, age, email, subjects) values(?, ?, ?, ?)";
 
     $stmt = $conn->prepare($SELECT);
     $stmt->bind_param("s", $email);
@@ -33,7 +29,7 @@ if(!empty($username)|| !empty($password)|| !empty($email)|| !empty($subjects)){
         $stmt->close();
 
         $stmt = $conn->prepare($INSERT);
-        $stmt->bind_param("ssssii", $username, $password, $email, $subjects);
+        $stmt->bind_param("siss", $username, $age, $email, $subjects);
         $stmt->execute();
         echo "New record inserted successfully";
     }
